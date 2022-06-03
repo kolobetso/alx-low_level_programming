0f1ab92e3685eb486c9ac64cebd4f095c3339797
George Malepeng <jaquar93@gmail.com>
2022-06-03 14:21:37 -0700

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,15 +3,16 @@
 /**   
 *main - print 0-9 separated with commas, using putchar
 *Return: Always 0 (Success)
- */
+*/
 
 int main(void)
 {
 		int n = '0';
-
+		
 		while (n <= '9')
 		{
 			putchar(n);
+
 			if (n != '9')
 			{
 				putchar(',');
