d03d5ffc34f6e2f1e62532b19230eef2d1adae04
George Malepeng <jaquar93@gmail.com>
2022-06-07 12:43:27 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,13 +8,12 @@
 void print_alphabet(void)
 {
 	char alpha;
-
 	alpha='a';
 
-	while(alpha <='z'){
-
+	while(alpha <='z')
+	{
 		_putchar(alpha);
 		alpha++;
 	}
-	_putchr('\n');
+	_putchar('\n');
 }
