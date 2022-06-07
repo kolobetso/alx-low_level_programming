9a1f5cf2d0523c3cdfec98ab85be05de55c6ec6c
George Malepeng <jaquar93@gmail.com>
2022-06-07 13:36:05 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,9 +10,9 @@ void print_alphabet_x10(void)
 
 	int count;
 
-	for ( count = 1 ; count <= 10 ; count++)
+	for (count = 1 ; count <= 10 ; count++)
 	{
-		for (alpha ='a' ; alpha <='z' ; alpha++)
+		for (alpha ='a' ; alpha <= 'z' ; alpha++)
 		{
 			_putchar(alpha);
 		}
