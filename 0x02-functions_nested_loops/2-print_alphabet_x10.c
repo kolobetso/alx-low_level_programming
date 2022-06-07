46da5514303da5861e20cb520846255886385992
George Malepeng <jaquar93@gmail.com>
2022-06-07 13:16:31 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*print_alphabet_x10 - print lowercase alphabet ten times
+*Return: 0
+*/
+void print_alphabet_x10(void)
+{
+	char alpha;
+
+	int count=0;
+
+	for( count ==1 ; count <=10 ; count++)
+	{	
+		_putchar(alpha);
+	}
+	_putchar('\n');
+
+}
