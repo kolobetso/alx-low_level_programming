04dc8e4799e89525afc5736833bb10ad8cb61c0a
George Malepeng <jaquar93@gmail.com>
2022-06-07 12:37:58 -0700

    taks 1

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,4 +9,10 @@
 */
 
 int _putchar(char c);
+
+/**                                                                                                                                              
+ *print_alphabet - print lowercase alphabet                                                                                                                               
+ */
+void print_alphabet(void);
+
 #endif
