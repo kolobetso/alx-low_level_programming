a37f285951568b0068ffa79596517d1af901936a
George Malepeng <jaquar93@gmail.com>
2022-06-07 17:05:45 -0700

    Task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,15 +13,12 @@ void print_to_98(int n)
 		printf("%d, ", n);
 		n++;
 	}
-
 	while (n > 98)
 	{
 		printf("%d, ", n);
 		n--;
 	}
-	
 	if (n == 98)
-	printf("%d", n);
-	
+		printf("%d", n);
 	_putchar('\n');
 }
