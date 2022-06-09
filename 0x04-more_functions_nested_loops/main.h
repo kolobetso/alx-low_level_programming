8e8dc9df70c2f752a881325974586e1fb0ebc21b
George Malepeng <jaquar93@gmail.com>
2022-06-09 14:24:06 -0700

    Task 0

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#ifndef  MAIN_H
+#define MAIN_H
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: character to be checked
+ * Return: 1 if true, 0 if false  
+ */
+
+int _isupper(int c);
+
+
+
+#endif
