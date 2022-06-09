e779275f36dd3ff078663de027240dd4541b183f
George Malepeng <jaquar93@gmail.com>
2022-06-09 14:55:43 -0700

    Task 2

--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,2 +1,4 @@
 1.0  function that checks for uppercase character
 1.1  a function that checks for a digit (0 through 9).
+1.2  a function that multiplies two integers.
+
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -16,4 +16,12 @@ int _isupper(int c);
  */  
 int _isdigit(int c);
 
+/**
+ * mul - multiplies two integers
+ *@a: one integer
+ *@b: second integer
+ *Return: product of a and b
+ */
+int mul(int a, int b);
+
 #endif
