a4b4cb17b178a161f9e67e6e5478fc04ba049a90
George Malepeng <jaquar93@gmail.com>
2022-06-09 17:27:11 -0700

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,7 +11,7 @@ int main(void)
 	int i;
 
 	for (i = 1; i <= 100; i++)
-	{	
+	{
 		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz");
 		else if (i % 3 == 0)
@@ -20,9 +20,11 @@ int main(void)
 			printf("Buzz");
 		else
 			printf("%d", i);
+
 		if (i < 100)
 			printf(" ");
 	}
+
 	printf("\n");
 	return (0);
 }
