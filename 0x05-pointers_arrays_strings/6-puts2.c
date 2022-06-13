746380fcf8707c106fc9be612a090620c58bccf9
George Malepeng <jaquar93@gmail.com>
2022-06-13 13:31:37 -0700

    Task 5

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -23,12 +23,14 @@ int _strlen(char *s)
  * puts2 - prints every second character of a string, followed by a new line
  * @str: string to print
  */
+
 void puts2(char *str)
 {
 	int i;
 
 	for (i = 0; str[i] != '\0' && i < _strlen(str); i += 2)
 		_putchar(str[i]);
+
 
 	_putchar('\n');
 }
