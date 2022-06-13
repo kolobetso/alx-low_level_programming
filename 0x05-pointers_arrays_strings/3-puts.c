aaadbeab3a60ad7b494c331137301b1f7656a75c
George Malepeng <jaquar93@gmail.com>
2022-06-13 12:27:22 -0700

    Task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,14 +2,15 @@
 
 /**
  *_putchar - puts character to standard output
- * @c: character to put to standard output
+ *@c: character to put to standard output
+ *reutn 0
  */
 
 void _puts(char *s)
 {	
 	int i =0;
 
-	while (s[0])
+	while (s[i])
 	{
 		_putchar(s[i]);
 		i++;
