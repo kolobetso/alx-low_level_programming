2022fc7c00e652b906ae04513ed9ff452ef03a65
George Malepeng <jaquar93@gmail.com>
2022-06-15 11:56:40 -0700

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,16 +13,16 @@ char *_strcat(char *dest, char *src)
 	int i = 0;
 	int j = 0;
 
-	while (dest[i] != '\0') /*i is index of null terminator*/
+	while (dest[i] != '\0')/*i is index of null terminator*/
 		i++;
 
-	while (src[j] != '\0') /*append replacing null terminator*/
+	while (src[j] != '\0')/*append replacing null terminator*/
 	{
 		dest[i] = src[j];
 		i++;
 		j++;
 	}
-
 	dest[i] = '\0';
+	
 	return (dest);
 }
