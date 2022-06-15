bfb8499408f1ddf19762eafc13c29dae8c3cd486
George Malepeng <jaquar93@gmail.com>
2022-06-15 12:43:56 -0700

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -18,12 +18,10 @@ char *_strcat(char *dest, char *src)
 		length++;
 	
 	}
-
 	for (j = 0; src[j] != '\0'; j++, length++)
 	{
 		dest[length] = src[j];
 	}
 	dest[length] = '\0';
-	
 	return (dest);
 }
