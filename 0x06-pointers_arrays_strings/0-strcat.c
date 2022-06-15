7e05cb2bb091ac81a153b311bdfb62eaf7e8026f
George Malepeng <jaquar93@gmail.com>
2022-06-15 11:58:58 -0700

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _strncat - concatenate n bytes to destination string.
- * @dest: string to be appended to
- * @src: string to append
- * @n: append n number of bytes(chars)
- * Return: concatenated string
+ *_strncat - concatenate n bytes to destination string.
+ *@dest: string to be appended to
+ *@src: string to append
+ *@n: append n number of bytes(chars)
+ *Return: concatenated string
  */
 
 char *_strcat(char *dest, char *src)
@@ -13,10 +13,10 @@ char *_strcat(char *dest, char *src)
 	int i = 0;
 	int j = 0;
 
-	while (dest[i] != '\0')/*i is index of null terminator*/
+	while (dest[i] != '\0') /*i is index of null terminator*/
 		i++;
 
-	while (src[j] != '\0')/*append replacing null terminator*/
+	while (src[j] != '\0') /*append replacing null terminator*/
 	{
 		dest[i] = src[j];
 		i++;
