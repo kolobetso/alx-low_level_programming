4c169e368d432d825ad25ce6c56c884911ba63c3
George Malepeng <jaquar93@gmail.com>
2022-06-20 09:37:24 -0700

    Task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * Returns a pointer to the memory area s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int size = n;
+
+	if (size > 0)
+	{
+		int k=0;
+
+		for(k; k < size; k++)
+		{
+			s[k]=b;
+		}
+	}
+	return (s);
+}
