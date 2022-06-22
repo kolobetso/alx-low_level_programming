449318c7b61d5014225a2b783dc9c0bd5a11f1aa
George Malepeng <jaquar93@gmail.com>
2022-06-22 11:59:04 -0700

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * -puts_recursion.c to print on a new line
+ * _puts_recursion.c to print on a new line
  *  @s: dtring to put
  */
 
 void _puts_recursion(char *s)
 {
-	if(*s == '\0')
+	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
