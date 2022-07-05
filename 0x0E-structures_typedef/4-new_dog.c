0947003f085c7e97e61601a5cdbe3b389589cf9e
George Malepeng <jaquar93@gmail.com>
2022-07-05 12:51:48 -0700

    Task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -25,6 +25,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	for (lname = 0; name[lname]; lname++)
 		;
+
 	for (lowner = 0; owner[lowner]; lowner++)
 		;
 
@@ -36,11 +37,16 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(p_dog->owner);
 		free(p_dog->name);
 		free(p_dog);
-
 		return (NULL);
 	}
 
 	for (i = 0; i < lname; i++)
+		p_dog->name[i] = name[i];
+	p_dog->name[i] = '\0';
+
+	p_dog->age = age;
+
+	for (i = 0; i < lowner; i++)
 		p_dog->owner[i] = owner[i];
 	p_dog->owner[i] = '\0';
 
