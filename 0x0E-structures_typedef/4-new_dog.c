a78b20a06b31814f7224bb0048a266439b5d92ec
George Malepeng <jaquar93@gmail.com>
2022-07-05 12:57:59 -0700

    Task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,9 +1,8 @@
 #include "dog.h"
 #include <stdlib.h>
-
 /**
  * new_dog - creates a new dog.
- * @name: name of the dog.
+ * @name: name of the dog
  * @age: age of the dog.
  * @owner: owner of the dog.
  *
