997fd5941a3e233a9cfb87bfee554d43e8bacaed
George Malepeng <jaquar93@gmail.com>
2022-07-05 13:05:04 -0700

    Task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
+
 /**
  * new_dog - creates a new dog.
  * @name: name of the dog
@@ -9,13 +10,12 @@
  * Return: struct dog.
  * if fails, returns NULL.
  */
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *p_dog;
 	int i, lname, lowner;
 
-	p_dog = malloc(sizeof (*p_dog));
+	p_dog = malloc(sizeof(*p_dog));
 	if (p_dog == NULL || !(name) || !(owner))
 	{
 		free(p_dog);
