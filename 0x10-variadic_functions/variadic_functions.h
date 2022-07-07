2015d961cc16311497a5208d23d03cfceda15677
George Malepeng <jaquar93@gmail.com>
2022-07-07 12:24:22 -0700

    task 4

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,5 +4,5 @@
 void print_numbers(const char *separator, const unsigned int n, ...);
 int sum_them_all(const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
-
-#endif
+void print_all(const char * const format, ...);
+endif
