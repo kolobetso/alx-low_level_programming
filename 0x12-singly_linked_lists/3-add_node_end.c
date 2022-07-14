50e22c41290a850ec9636013186fbf67b99024e5
kolobetso <53598770+kolobetso@users.noreply.github.com>
2022-07-14 23:51:13 +0200

    Update 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -5,14 +5,12 @@
  * @str: string
  * Return: length
  */
-
 int _strlen(const char *str)
 {
 	int len;
 
 	for (len = 0; str[len] != '\0'; len++)
 		;
-
 	return (len);
 }
 
@@ -22,14 +20,13 @@ int _strlen(const char *str)
  * @str: data for new node
  * Return: address of new element, or NULL if failed
  */
-
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node, *tmp; /* create new node */
 
 	if (str == NULL) /* validate input */
 		return (NULL);
-	if (strdup(str) == NULL) /*check if strdup malloc errored */
+	if (strdup(str) == NULL) /* check if malloc errored */
 		return (NULL);
 
 	new_node = malloc(sizeof(list_t)); /* malloc for new node */
@@ -40,8 +37,8 @@ list_t *add_node_end(list_t **head, const char *str)
 	new_node->len = _strlen(str);
 	new_node->next = NULL;
 
-	if (head == NULL) /* set what new node points to first */
-		new_node->next = NULL;
+	if (*head == NULL) /* if no list nodes, set new_node to beginning */
+		*head = new_node;
 	else
 	{
 		tmp = *head;
