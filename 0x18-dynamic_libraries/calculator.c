f5514be4ae12a4cbedfd33ee1fa23e92c0335ae6
George Malepeng <jaquar93@gmail.com>
2022-09-05 14:35:34 -0700

    Task 2

---
+++ 0x18-dynamic_libraries/calculator.c
@@ -0,0 +1,51 @@
+/**
+ * add fun - add two variable
+ * @a: variable 1
+ * @b: variable 2
+ * return: sum
+ */
+int  add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - subtratcs two variables
+ * @a: variable 1
+ * @b: variable 2
+ * Return: difference
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - multiply two variable
+ * @a: variable 1
+ * @b: variable 2
+ * Return: product of two numbers
+ */
+int mul(int a, int b)
+{
+	return ( a * b);
+}
+/**
+ * div - divede two numbers
+ * @a: number 1
+ * @b : number 2
+ * Return: qoetent of two numbers
+ */
+int div(int a, int b)
+{
+       	return (a / b);
+}
+
+/**
+ * mod - modulus of two variables
+ * @: value 1
+ * @b: value 2
+ * Return: reminder of two numbers
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
